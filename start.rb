ee73c092d9fb8dae18c518028122255ac93b7762
PaulaEs <paulaproxy@gmail.com>
2014-04-30 23:03:37 +0100

    Simple urls.

--- start.rb
+++ start.rb
@@ -4,3 +4,17 @@ require 'shotgun'
 get '/' do
   "That's a test."
 end
+
+get '/about' do
+    "Something about me."
+end
+
+get '/hello/:name' do
+  "Hello #{params[:name]}! How are you?"
+end
+
+get '/more/*' do
+  # That a wildcard
+  params[:splat]
+end
+
