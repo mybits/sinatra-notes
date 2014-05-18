db225fc123edcdd860d0000c39363976f1664685
PaulaEs <paulaproxy@gmail.com>
2014-05-18 01:28:45 +0100

    Adding route for a put request.

--- notes/recall.rb
+++ notes/recall.rb
@@ -36,3 +36,12 @@ get '/:id' do
   @title = "Edit note ##{params[:id]}"
   erb :edit
 end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
